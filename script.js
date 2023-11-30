dee44403fa18e3ea617cdeec18040bb3529c8d99
evanderzzstudio <evanderstudio@gmail.com>
2023-11-30 10:42:12 +0700

    - Update SEARCH_API and search functionality.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a20d8af5b75a461d3e3fa63cd3fc6ff7&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500s'
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?api_key=a20d8af5b75a461d3e3fa63cd3fc6ff7&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=a20d8af5b75a461d3e3fa63cd3fc6ff7&query="'
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
@@ -21,7 +21,9 @@ form.addEventListener('submit', (e) => {
 
     const searchTerm = search.value
 
-    if (searchTerm) {
+    if (searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
 
-    }
+        search.value = ''
+    } else { }
 })
