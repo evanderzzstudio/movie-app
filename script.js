4986e2dfb2b4fda23c82e35af0b6778ecd1c8936
evanderzzstudio <evanderstudio@gmail.com>
2023-11-30 11:08:59 +0700

    - Refactor showMovies function and remove unnecessary console logs.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w500s'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=a20d8af5b75a461d3e3fa63cd3fc6ff7&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -12,7 +13,18 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results)
+    showMovies(data.results)
+}
+
+function showMovies(movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+    })
 }
 
 form.addEventListener('submit', (e) => {
@@ -25,5 +37,7 @@ form.addEventListener('submit', (e) => {
         getMovies(SEARCH_API + searchTerm)
 
         search.value = ''
-    } else { }
+    } else {
+        window.location.reload()
+    }
 })
