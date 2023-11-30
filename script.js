e67fe69d602ffcb414dbc8be1305dcbb4f8864e0
evanderzzstudio <evanderstudio@gmail.com>
2023-11-30 11:39:17 +0700

    The commit message states that the movie elements in the HTML file have been updated, including the IMG_PATH and movie element template.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a20d8af5b75a461d3e3fa63cd3fc6ff7&page=1'
-const IMG_PATH = 'https://image.tmdb.org/t/p/w500s'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=a20d8af5b75a461d3e3fa63cd3fc6ff7&query="'
 
 const main = document.getElementById('main')
@@ -24,7 +24,33 @@ function showMovies(movies) {
 
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+        <div class="movie">
+            <img src="${IMG_PATH + poster_path}"
+                alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+               ${overview} 
+            </div>
+        </div>
+        `
+        main.appendChild(movieEl)
     })
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return 'green'
+    } else if (vote >= 5) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
 }
 
 form.addEventListener('submit', (e) => {
